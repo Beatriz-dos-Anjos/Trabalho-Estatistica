283d9a217e2c50297983a5d8a3bbc4617bebc12d
paulogestosa <pemiliogvieira@live.com>
2024-07-04 03:24:49 -0300

    test01 - matplotlib

---
+++ test.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+
+# Dados de exemplo
+x = [1, 2, 3, 4, 5]
+y = [10, 15, 13, 18, 16]
+
+# Criar o gráfico
+plt.plot(x, y)
+
+# Adicionar título e rótulos
+plt.title('Exemplo de Gráfico de Linha')
+plt.xlabel('Eixo X')
+plt.ylabel('Eixo Y')
+
+# Mostrar o gráfico
+plt.show()
